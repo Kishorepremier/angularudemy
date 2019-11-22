767548bf40084225e2c4e2f73fb41ad04672355b
Kishore <kishorepremier@gmail.com>
2019-11-22 17:21:33 +0530

    Initial commit

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +16,25 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts()
-  {
+  getPosts(){
    return this.http.get(this.url);
   }
 
-  createPost()
-  {
-    return this.http.post(this.url, JSON.stringify(post))
+  createPost(post){
+    return this.http.post(this.url, JSON.stringify(post));
   }
 
-  updatePost()
-  {
-    this.http.delete(this.url + '/' + post.id)
+  updatePost(post){
+    return this.http.delete(this.url + '/' + post.id);
   }
 
-  deletepost()
-  {
-    this.http.delete(this.url + '/' + post.id)
+  deletePost(id){
+    return this.http.delete(this.url + '/' + id)
+    .catch((error: Response) => {
+      if(error  === 404)
+      return Observable.throw (new NotFoundError());
+      return Observable.throw (new AppError(error));
+      
+    })
   }
 }
