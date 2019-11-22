934a2858b650345e515c3404968c8538c6b2b60f
Kishore <kishorepremier@gmail.com>
2019-11-22 17:20:57 +0530

    Initial commit

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,51 +1,75 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/post.service';
+import { AppError } from './common/app-error';
+import { NotFoundError } from '../common/not-found-error';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html'
 })
 export class PostsComponent implements OnInit {
-  
   posts: any[];
-  
-  constructor(private service: PostService)
-  {
-    
+  constructor(private service: PostService) { }
 
-  }
   ngOnInit() {
     this.service.getPosts()
-    .subscribe(response => {
-      this.posts = response;
+      .subscribe(response => {
+        this.posts = response;
 
-    });
+      },
+        error => {
+          alert('An unexpected error occured');
+          console.log(error);
+        });
   }
 
-  createPost(input: HTMLInputElement)
-  {
+  createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
     this.service.createPost(post)
-    .subscribe(response => {
-      post['id'] = response;
-      this.posts.splice(0, 0, post);
-    });
+      .subscribe(
+        response => {
+          post['id'] = response;
+          this.posts.splice(0, 0, post);
+        },
+        (error: Response) => {
+          if(error.status ===400) {
+         // this.form.setErrors(error.json());
+          }
+          else {
+          alert('Problem in creating the post');
+          console.log(error);
+        }
+        });
   }
-  updatePost(post)
-  {
-    this.service.updatePost
-    .subscribe(response => {
-      console.log(response);
-    })
+
+  updatePost(post) {
+    this.service.updatePost(post)
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          alert('Problem in updating the post');
+          console.log(error);
+        });
   }
-  deletePost(post)
-  {
-    this.serive.deletePost()
-    .subscribe(response => {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-    })
+
+  deletePost(post) {
+    this.service.deletePost(345)
+      .subscribe(
+        response => {
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+        },
+        (error: AppError) => {
+          if (error instanceof NotFoundError) {
+            alert('This post has already been deleted');
+          } else {
+            alert('Problem in deleting the post');
+            console.log(error);
+          }
+        });
   }
 
 }
